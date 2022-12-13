270dc8970cd4b040567353ccfc876e2a577c6bb3
Aldhair Cabañas <aldhair.escobar@gmail.com>
2022-12-12 23:10:14 -0600

    adding data attibutes - handleclickfunction - document eventlistener

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ tweetBtn.addEventListener('click', function(){
   console.log(tweetInput.value)
 })
 
+document.addEventListener('click', function(e){
+  if(e.target.dataset.like){
+    handleLikeClick(e.target.dataset.like)
+  }
+})
+
+function handleLikeClick(tweetId){
+  const targetTweetObj = tweetsData.filter(function(tweet) {
+    return tweet.uuid === tweetId
+  })[0]
+
+  targetTweetObj.likes++
+  console.log(targetTweetObj.likes)
+}
+
 function getFeedHtml(){
 
   let feedHtml = ``
@@ -21,15 +36,15 @@ function getFeedHtml(){
           <p class="tweet-text">${tweet.tweetText}</p>
           <div class="tweet-details">
             <span class="tweet-detail">
-              <i class="fa-regular fa-comment-dots"></i>
+              <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
               ${tweet.replies.length}
             </span>
             <span class="tweet-detail">
-              <i class="fa-solid fa-heart"></i>
+              <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
               ${tweet.likes}
             </span>
             <span class="tweet-detail">
-              <i class="fa-solid fa-heart"></i>
+              <i class="fa-solid fa-heart" data-retweet="${tweet.uuid}"></i>
               ${tweet.retweets}
             </span>
           </div>   
