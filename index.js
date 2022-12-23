c96e68b57584cd437a7d49f4ed66feda97bd7ca6
Aldhair Cabañas <aldhair.escobar@gmail.com>
2022-12-22 18:15:41 -0600

    stretch goals - reply a tweet - delete a tweet

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { tweetsData } from './data.js'
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-const tweetInput = document.getElementById('tweet-input')
-
 document.addEventListener('click', function(e){
   if (e.target.dataset.like){
     handleLikeClick(e.target.dataset.like)
@@ -13,8 +11,14 @@ document.addEventListener('click', function(e){
   else if (e.target.dataset.reply){
     handleReplyClick(e.target.dataset.reply)
   }
+  else if (e.target.dataset.delete){
+    handleDeleteClick(e.target.dataset.delete)
+  }
   else if (e.target.id === 'tweet-btn'){
     handleTweetBtnClick()
+  }
+  else if (e.target.id === 'reply-btn'){
+    handleReplyBtnClick(e.target.dataset.replyTweet)
   }
 
 })
@@ -51,25 +55,72 @@ function handleRetweetClick(tweetId){
   render()
 }
 
+function handleDeleteClick(tweetId){
+
+  const objWithDataIndex = tweetsData.findIndex(function(obj){
+    return obj.uuid === tweetId
+  })
+
+  console.log(objWithDataIndex)
+
+  tweetsData.splice(objWithDataIndex, 1)
+
+  /* tweetsData.filter(function(tweet){
+    return tweet.uuid != tweetId
+  }) */
+
+  render()
+}
+
 function handleReplyClick(replyId){
   document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
 function handleTweetBtnClick(){
+  const tweetInput = document.getElementById('tweet-input')
 
-  tweetsData.unshift({
-    handle: `Twimba`,
-    profilePic: `images/scrimbalogo.png`,
-    likes: 0,
-    retweets: 0,
-    tweetText: tweetInput.value,
-    replies: [],
-    isLiked: false,
-    isRetweeted: false,
-    uuid: uuidv4()
+  if (tweetInput.value){
+
+    tweetsData.unshift({
+      handle: `Twimba`,
+      profilePic: `images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      canDelete: true,
+      uuid: uuidv4()
+    })
+    
+    render()
+
+    tweetInput.value = ''
+
+  }
+}
+
+function handleReplyBtnClick(tweetId){
+  let replyMessage = document.getElementById(`reply-input-${tweetId}`)
+
+  tweetsData.forEach(function(tweet){
+    if (tweet.uuid === tweetId){
+
+      const newReplyObj = {
+        handle: `Twimba`,
+        profilePic: `images/scrimbalogo.png`,
+        tweetText: replyMessage.value,
+      }
+
+      tweet.replies.unshift(newReplyObj)
+
+      replyMessage.value = ''
+
+      render()
+    }
   })
-  
-  render()
+
 
 }
 
@@ -91,7 +142,29 @@ function getFeedHtml(){
       retweetIconClass = 'retweeted'
     }
 
-    let repliesHtml = ''
+    let deleteIconHtml = ''
+
+    if (tweet.canDelete){
+      deleteIconHtml = `
+      <span class="tweet-detail">
+        <i class="fa-solid fa-trash" data-delete="${tweet.uuid}"></i>
+      </span>
+      `
+    }
+
+    let repliesHtml = `
+    <div class="reply-container">
+      <div class="reply-inner">
+        <img src="images/scrimbalogo.png" class="profile-pic-reply">
+        <textarea 
+				placeholder="Tweet your reply" 
+				id="reply-input-${tweet.uuid}"
+        class="reply-input"
+			  ></textarea>
+        <button id="reply-btn" data-reply-tweet="${tweet.uuid}">Reply</button>
+      </div>
+    </div>
+    `
 
     if (tweet.replies.length > 0){
 
@@ -131,6 +204,7 @@ function getFeedHtml(){
               <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
               ${tweet.retweets}
             </span>
+            ${deleteIconHtml}            
           </div>   
         </div>            
       </div>
